55c46ccdd805915422b3cde2f8edcf96b9eae91e
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-10-24 00:07:48 +0530

    Datatypes

--- 01_basics /03_dataypes.py
+++ 01_basics /03_dataypes.py
@@ -7,3 +7,19 @@
 #tuple (tuple - ordered, immutable collection of items) e.g., (1, 2, 3), ('x', 'y', 'z')
 #dictionary (dict - key-value pairs) e.g., {'name': 'Alice', 'age': 25}
 #set (set - unordered collection of unique items) e.g., {1, 2,
+age = 20 # interger
+print(age)
+print(type(age))
+
+cgpa = 8.5 # float
+print(cgpa)
+print(type(cgpa))
+
+string = ("Hello, Python!") # string
+print(string)
+print(type(string))
+
+is_student = True # boolean this can be either true or false
+print(is_student)   
+print(type(is_student))
+
