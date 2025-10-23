b22429ed2c7d76e3bac3cf1cbda8a25b807e74da
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-10-24 00:04:59 +0530

    typecasting

---
+++ 01_basics /03_dataypes.py
@@ -0,0 +1,9 @@
+#python support various data types
+# integer (int  - whole numbers) e.g., 1, 2, 3
+# float (floating point numbers - decimal numbers) e.g., 1.5, 2.75
+# string (str - sequence of characters) e.g., "Hello", 'Python'
+# boolean (bool - True or False) e.g., True, False      
+#list (list - ordered collection of items) e.g., [1, 2, 3], ['a', 'b', 'c']
+#tuple (tuple - ordered, immutable collection of items) e.g., (1, 2, 3), ('x', 'y', 'z')
+#dictionary (dict - key-value pairs) e.g., {'name': 'Alice', 'age': 25}
+#set (set - unordered collection of unique items) e.g., {1, 2,
---
+++ 02_typecasting /01_typecasting.py
@@ -0,0 +1,10 @@
+a = 24 
+b = "24"
+print(a)
+print(type(a)) 
+print(b)
+print(type(b)) 
+# Typecasting convert b to be an integer
+b = int(b)
+print(b)
+print(type(b))
