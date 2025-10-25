13f1285c579f08fab68c6a3f8500a373add09ef8
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-10-25 09:27:00 +0530

    comments_print_esc_properties

---
+++ 03_prints.py
@@ -0,0 +1,16 @@
+print("hello world", "Anii")  
+print('hello world', 'Anii')# single and double quotes both are used to print string in python
+print(""" hello world
+Anii """)  # triple quotes are used to print multi line strings in python
+print(''' hello world
+Anii ''')  # triple quotes are used to print multi line strings in python
+print(1234)  # integer value is printed
+print(12.34)  # float value is printed
+print(True)  # boolean value True is printed
+print(False)  # boolean value False is printed
+print(None)  # None value is printed        # None value is printed                 
+
+print("Hello", end="***")  # end parameter is used to change the default behavior of print function
+print("World")  # by default end parameter is \n (new line)
+print("Hello", sep="***", end="@@@")  # sep parameter is used to change the default behavior of print function
+print("World", "Anii")  # by default sep parameter is space (" ")       
